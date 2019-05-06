e628f6f9af741358553c0bf660dea90f6fedc539
wilker <364105996@qq.com>
2019-05-06 15:46:16 +0800

    a

---
+++ Assets/ShaderGraph/CustomNode/MyCustomNode.cs
@@ -0,0 +1,24 @@
+﻿using System.Reflection;
+using UnityEditor.ShaderGraph;
+using UnityEngine;
+
+[Title("Custom", "My Custom Node1")] // 创建节点时 节点所在的组
+public class MyCustomNode : CodeFunctionNode {
+    public MyCustomNode() {
+        name = "My Custom Node2";
+    }
+
+    protected override MethodInfo GetFunctionToConvert() {
+        return GetType().GetMethod("MyCustomFunction",
+            BindingFlags.Static | BindingFlags.NonPublic);
+    }
+
+    static string MyCustomFunction(
+        [Slot(0, Binding.None)] DynamicDimensionVector A, [Slot(1, Binding.None)] DynamicDimensionVector B, [Slot(2, Binding.None)] out DynamicDimensionVector Out) {
+        return @"
+{
+	Out = A + B;
+} 
+";
+    }
+}
